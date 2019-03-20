a72bf6f4b7975dac56ccdc13a79647a8dc4c3d90
Shital Jadhav <shital.jadhavt11@gmail.com>
2019-03-20 15:46:54 +0100

    Added Custom table cell of news list

---
+++ NewsAPI/View/NewsCustomCell.swift
@@ -0,0 +1,42 @@
+//
+//  NewsCustomCell.swift
+//  NewsAPI
+//
+//  Created by Tushar  Jadhav on 2019-03-20.
+//  Copyright © 2019 Shital  Jadhav. All rights reserved.
+//
+
+import UIKit
+
+class NewsCustomCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var sourceLabel: UILabel!
+    @IBOutlet weak var newsIconImageView: UIImageView!
+    @IBOutlet weak var postDateLabel: UILabel!
+    
+    var model: Article? {
+        didSet {
+            guard let model = model else {
+                return
+            }
+        
+            titleLabel.text = model.title
+            sourceLabel.text = model.source.name
+            postDateLabel.text = model.publishedAt.getFeedPostDate()
+            
+        }
+    }
+   
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        newsIconImageView.clipsToBounds = true
+        newsIconImageView.layer.cornerRadius = 3.0
+        
+    }
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
