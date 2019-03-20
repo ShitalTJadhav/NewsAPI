1c81b7d2c40ea09180b824a9f651c5b2dc7c9f6b
Shital Jadhav <shital.jadhavt11@gmail.com>
2019-03-20 22:00:54 +0100

    Adding feed service network class

---
+++ NewsAPI/Service/FeedsService.swift
@@ -0,0 +1,44 @@
+//
+//  FeedsService.swift
+//
+//  Created by Tushar  Jadhav on 2019-03-03.
+//  Copyright © 2019 Shital  Jadhav. All rights reserved.
+//
+
+import Foundation
+
+final class FeedsService {
+    
+    private let networking: NetworkingProtocol?
+    
+    init(networking: NetworkingProtocol?) {
+        self.networking = networking
+    }
+    
+    /// Fetch all activities that happened between from and to date
+    ///
+    /// - Parameter completion: Called when operation finishes
+    func fetchNews(fromDate: String, toDate: String, completion: @escaping (Result<NewsResponceModel, DataResponseError>) -> Void) {
+        
+        let resource = Resource(url: NetworkService.shared.baseURL, path: "", parameters: [
+            "q": "apple",
+            "from": fromDate,
+            "to": toDate,
+            "sortBy":"popularity",
+            "apiKey":NetworkService.shared.apiKey
+            ])
+        
+        _ = networking?.fetch(resource: resource, completion: { data in
+            
+            if let error = data as? Error {
+                completion(Result.failure(DataResponseError.custom(error.localizedDescription)))
+                return
+            }
+            
+    
+            //Parse data and get result
+            return ParseHelper.parseData(data: data as? Data, completion: completion)
+        })
+    }
+
+}
