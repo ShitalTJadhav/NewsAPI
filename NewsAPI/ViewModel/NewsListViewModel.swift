34af58e754d59d18882d39f1b9e6d813ba013c27
Shital Jadhav <shital.jadhavt11@gmail.com>
2019-03-20 21:52:29 +0100

    Adding ViewModel of newslist and api call from network service

---
+++ NewsAPI/ViewModel/NewsListViewModel.swift
@@ -0,0 +1,62 @@
+//
+//  NewsListViewModel.swift
+//  NewsAPI
+//
+//  Created by Tushar  Jadhav on 2019-03-20.
+//  Copyright © 2019 Shital  Jadhav. All rights reserved.
+//
+
+import Foundation
+
+class NewsListViewModel {
+    
+    weak var service :FeedsService?
+    var dataList : [Article] = []
+    
+    // date indicating from 'from' fetch all news
+    var fromDate : String = ""
+    
+    // date indicating from 'to' fetch all news
+    var toDate : String = ""
+    
+    init(service : FeedsService) {
+        self.service = service
+        
+        toDate = toDate.getCurrentDate()
+    }
+    
+    var totalNewsCount: Int {
+        return dataList.count
+    }
+    
+    func articel(at index: Int) -> Article {
+        return dataList[index]
+    }
+    
+    func loadNewsData(_ completion: @escaping (Result<[Article], DataResponseError>) -> Void) {
+        
+        guard let _ = service else {
+            completion(Result.failure(DataResponseError.custom("Missing service")))
+            return
+        }
+        
+        self.service?.fetchNews(fromDate: fromDate, toDate: toDate, completion: { [weak self] result in
+            
+            switch result {
+                
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    completion(Result.failure(error))
+                }
+            case .success(let response):
+                
+                self?.dataList = response.articles
+                
+                DispatchQueue.main.async {
+                    completion(Result.success(response.articles))
+                }
+            }
+        })
+    }
+    
+}
